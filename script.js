2c5aa947c462a45a097ce750dbdf7d41dbc205c4
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-06-17 19:42:58 +0300

    Fibonacci sequence complete

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
+let number = parseInt(prompt('Please enter Fibonacci number you want to find, negative values allowed', '42'))
 
-document.getElementById('main').innerHTML = `The factorial of ${number} is: ${factorial(number)}`;
+let fib = (f0 = 0, f1 = 1, n) => {
+    if (n < 0) {
+        return (fib (f1-f0, f0, n+1))
+    } else if (n === 0) {
+        return f0;
+    } else {
+        return (fib (f1, f0+f1, n-1));
+    }
+}
+
+
+document.getElementById('main').innerHTML = `The Fibonacci ${number}th number is: ${fib(0, 1, number)}`;
